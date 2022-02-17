244fdc370f7b8e3330ac8167619cb0778340d430
mohitgupta-byte <sahilbakoru1999@gmail.com>
2022-02-17 22:09:38 +0530

    dd

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
-const { STRIPE_SECRET } = require("../src/utils/common")
+//const { STRIPE_SECRET } = require("../src/utils/common")
 // const stripe = require('stripe')("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");
-const stripeTest = require('stripe')(STRIPE_SECRET);
+//const stripeTest = require('stripe')(STRIPE_SECRET);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
